d623cb943690ddc0e711ba6dc67ad56d023424a8
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2024-12-05 07:40:06 +0100

    solve part 2 5. dec + cleanup

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -17,38 +17,31 @@ fn parse(input: &str) -> (Vec<(i64, i64)>, Vec<Vec<i64>>) {
     (rules, updates)
 }
 
-fn part_1_check_update(after_sets: &mut HashMap<i64, HashSet<i64>>, before_sets: &mut HashMap<i64, HashSet<i64>>, update: &[i64]) -> bool {
-    for (i, page) in update.iter().enumerate() {
-        let before = &update[..i];
-        let after = &update[i+1..];
-        let after_set = after_sets.entry(*page).or_default();
-        let before_set = before_sets.entry(*page).or_default();
-        if before.iter().any(|before_page| after_set.contains(before_page)) {
-            return false
-        }
-        if after.iter().any(|after_page| before_set.contains(after_page)) {
-            return false
-        }
+fn check_update(after_sets: &HashMap<i64, HashSet<i64>>, update: &[i64]) -> bool {
+    ! update.iter()
+        .enumerate()
+        .filter_map(|(i, page)| after_sets.get(page).map(|set| (i, set)))
+        .any(|(i, set)| update[..i].iter().any(|page| set.contains(page)))
+}
+
+fn preprocess_rules(rules: &[(i64, i64)]) -> HashMap<i64, HashSet<i64>> {
+    let mut after_sets: HashMap<i64, HashSet<i64>> = HashMap::new();
+
+    for &(before, after) in rules {
+        after_sets.entry(before).or_default().insert(after);
     }
 
-    true
+    after_sets
 }
 
 fn part_1(input: &str) -> i64 {
     let (rules, updates) = parse(input);
+    let after_sets = preprocess_rules(&rules);
 
     let mut middle_sum = 0;
 
-    let mut after_sets: HashMap<i64, HashSet<i64>> = HashMap::new();
-    let mut before_sets: HashMap<i64, HashSet<i64>> = HashMap::new();
-
-    for &(before, after) in &rules {
-        after_sets.entry(before).or_default().insert(after);
-        before_sets.entry(after).or_default().insert(before);
-    }
-
     for update in &updates {
-        if part_1_check_update(&mut after_sets, &mut before_sets, &update) {
+        if check_update(&after_sets, &update) {
             middle_sum += update[update.len() / 2];
         }
 
@@ -57,8 +50,42 @@ fn part_1(input: &str) -> i64 {
     middle_sum
 }
 
+fn part_2(input: &str) -> i64 {
+    let (rules, updates) = parse(input);
+    let after_sets = preprocess_rules(&rules);
+
+    let mut middle_sum = 0;
+
+    for update in &updates {
+        if check_update(&after_sets, &update) {
+            continue;
+        }
+
+        let mut pages: HashSet<i64> = HashSet::from_iter(update.iter().cloned());
+        let mut new_update = vec![];
+
+        while ! pages.is_empty() {
+            let hash_set = pages.clone();
+            for page in hash_set.iter() {
+                if pages.iter().filter_map(|other_page| after_sets.get(other_page)).any(|after_set| after_set.contains(page)) {
+                    continue;
+                }
+                pages.remove(page);
+                new_update.push(*page);
+                break
+            }
+        }
+
+        assert!(check_update(&after_sets, &new_update));
+
+        middle_sum += new_update[update.len() / 2];
+    }
+
+    middle_sum
+}
+
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
     println!("part_1 = {}", part_1(input.as_str()));
-    println!("Hello, world!");
+    println!("part_2 = {}", part_2(input.as_str()));
 }
