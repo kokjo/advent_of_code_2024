d67d85031090ad507bc663017409b4560a4078db
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2024-12-07 20:49:37 +0100

    add fast solution to 7.dec

--- 7/src/main.rs
+++ 7/src/main.rs
@@ -42,6 +42,28 @@ fn part_1(input: &str) -> i64 {
     sum
 }
 
+fn check_part_1_fast(result: i64, numbers: &[i64]) -> bool {
+    if let Some((&last, rest)) = numbers.split_last() {
+        if result % last == 0 && check_part_1_fast(result / last, rest) {
+            true
+        } else if result >= last && check_part_1_fast(result - last, rest) {
+            true
+        } else {
+            false
+        }
+    } else {
+        result == 0
+    }
+}
+
+fn part_1_fast(input: &str) -> i64 {
+    parse(input)
+        .iter()
+        .filter(|(result, numbers)| check_part_1_fast(*result, numbers))
+        .map(|(result, _)| result)
+        .sum()
+}
+
 #[derive(Copy, Clone, PartialEq, Eq, Ord, PartialOrd)]
 enum Op2 { Add, Mul, Cat }
 
@@ -59,6 +81,7 @@ fn part_2(input: &str) -> i64 {
     let mut sum = 0;
     for (result, numbers) in parse(input) {
         let mut ops = vec![Op2::Add; numbers.len()-1];
+
         for _ in 0..3usize.pow(ops.len() as u32) {
             for i in 0..ops.len() {
                 let (carry, next) = ops[i].inc();
@@ -81,9 +104,39 @@ fn part_2(input: &str) -> i64 {
     sum
 }
 
+fn check_part_2_fast(result: i64, numbers: &[i64]) -> bool {
+    if let Some((&last, rest)) = numbers.split_last() {
+        if result % last == 0 && check_part_2_fast(result / last, rest) {
+            true
+        } else if result >= last && check_part_2_fast(result - last, rest) {
+            true
+        } else {
+            let digits = 10i64.pow(last.checked_ilog10().unwrap()+1);
+            if result % digits == last && check_part_2_fast(result / digits, rest) {
+                true
+            } else {
+                false
+            }
+        }
+    } else {
+        result == 0
+    }
+}
+
+fn part_2_fast(input: &str) -> i64 {
+    parse(input)
+        .iter()
+        .filter(|(result, numbers)| check_part_2_fast(*result, numbers))
+        .map(|(result, _)| result)
+        .sum()
+}
+
+
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
 
     println!("part_1 = {}", part_1(&input));
+    println!("part_1_fast = {}", part_1_fast(&input));
     println!("part_2 = {}", part_2(&input));
+    println!("part_2_fast = {}", part_2_fast(&input));
 }
