1cf1d621c58e81791492781e2208cfc7072556f4
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2024-12-23 16:08:49 +0100

    Much better solution for 23. dec

--- 23/src/main.rs
+++ 23/src/main.rs
@@ -1,70 +1,56 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, BTreeSet};
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct UnorderedPair<'a>(&'a str, &'a str);
+type Graph<'a> = HashMap<&'a str, BTreeSet<&'a str>>;
 
-impl<'a> UnorderedPair<'a> {
-    fn new(a: &'a str, b: &'a str) -> Self {
-        Self(a.min(b), a.max(b))
-    }
-}
-
-fn parse(input: &str) -> HashSet<UnorderedPair> {
+fn parse(input: &str) -> Graph {
+    let mut adjs = Graph::new();
     input
         .trim()
         .lines()
-        .filter_map(|line| {
-            line.trim()
-                .split_once('-')
-                .map(|(a, b)| UnorderedPair::new(a, b))
-        })
-        .collect()
+        .filter_map(|line| line.trim().split_once('-'))
+        .for_each(|(a, b)| {
+            adjs.entry(a).or_default().insert(b);
+            adjs.entry(b).or_default().insert(a);
+        });
+    adjs
 }
 
-fn is_clique(pairs: &HashSet<UnorderedPair>, clique: &[&str]) -> bool {
-    if let Some((&first, rest)) = clique.split_first() {
-        rest.iter().all(|&other| pairs.contains(&UnorderedPair::new(first, other)))
-            && is_clique(pairs, rest)
-    } else {
-        true
-    }
-}
-
-fn part_1(pairs: &HashSet<UnorderedPair>) -> usize {
-    let nodes: HashSet<&str> = pairs.iter().flat_map(|pair| [pair.0, pair.1]).collect();
-
-    let mut sets_of_3 = HashSet::<[&str; 3]>::new();
-    for &a in nodes.iter() {
-        if ! a.starts_with('t') {
-            continue;
-        }
-        for &b in nodes.iter() {
-            for &c in nodes.iter() {
-                let mut triple = [a, b, c];
-                if is_clique(pairs, &triple) {
-                    triple.sort();
-                    sets_of_3.insert(triple);
+fn part_1(graph: &Graph) -> usize {
+    let mut clique3s = BTreeSet::new();
+    for (&a, a_adj) in graph.iter() {
+        if a.starts_with('t') {
+            for &b in a_adj.iter() {
+                if let Some(b_adj) = graph.get(b) {
+                    for &c in a_adj.intersection(b_adj) {
+                        let mut clique3 = [a, b, c];
+                        clique3.sort();
+                        clique3s.insert(clique3);
+                    }
                 }
             }
         }
     }
-
-    sets_of_3.len()
+    clique3s.len()
 }
 
-fn build_largest_clique<'a>(pairs: &HashSet<UnorderedPair<'a>>, mut clique: Vec<&'a str>, adjs: &[&'a str]) -> Vec<&'a str> {
-    if let Some((&first, rest)) = adjs.split_first() {
-        let without_first = build_largest_clique(pairs, clique.clone(), rest);
+fn build_largest_clique<'a>(graph: &Graph, mut clique: Vec<&'a str>, mut adjs: BTreeSet<&'a str>, cur_best_size: usize) -> Vec<&'a str> {
+    if clique.len() + adjs.len() <= cur_best_size {
+        return vec![]
+    }
+    if let Some(first) = adjs.pop_first() {
+        let without_first = build_largest_clique(graph, clique.clone(), adjs.clone(), cur_best_size);
+
+        if let Some(first_adjs) = graph.get(first) {
+            adjs.retain(|&adj| first_adjs.contains(adj));
+        } else {
+            adjs.clear()
+        }
 
         clique.push(first);
-        if is_clique(pairs, &clique) {
-            let with_first = build_largest_clique(pairs, clique, rest);
 
-            if with_first.len() > without_first.len() {
-                with_first
-            } else {
-                without_first
-            }
+        let with_first = build_largest_clique(graph, clique, adjs, cur_best_size);
+        if with_first.len() > without_first.len() {
+            with_first
         } else {
             without_first
         }
@@ -73,14 +59,11 @@ fn build_largest_clique<'a>(pairs: &HashSet<UnorderedPair<'a>>, mut clique: Vec<
     }
 }
 
-fn part_2(pairs: &HashSet<UnorderedPair>) -> String {
-    let nodes: HashSet<&str> = pairs.iter().flat_map(|pair| [pair.0, pair.1]).collect();
-
+fn part_2(graph: &Graph) -> String {
     let mut best_clique = vec![];
 
-    for &node in nodes.iter() {
-        let adjs: Vec<&str> = pairs.iter().filter_map(|pair| (pair.0 == node).then(|| pair.1).or((pair.1 == node).then(|| pair.0))).collect();
-        let clique = build_largest_clique(pairs, vec![node], &adjs);
+    for (node, adjs) in graph.iter() {
+        let clique = build_largest_clique(graph, vec![node], adjs.clone(), best_clique.len());
         if clique.len() > best_clique.len() {
             best_clique = clique;
         }
@@ -92,8 +75,8 @@ fn part_2(pairs: &HashSet<UnorderedPair>) -> String {
 
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
-    let pairs = parse(&input);
+    let graph = parse(&input);
 
-    println!("part_1 = {}", part_1(&pairs));
-    println!("part_2 = {}", part_2(&pairs));
+    println!("part_1 = {}", part_1(&graph));
+    println!("part_2 = {}", part_2(&graph));
 }
